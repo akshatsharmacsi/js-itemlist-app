ecba56cfa8040ccdaf6f3d35744c2ec3995f2853
Akshat Sharma <akshatsharmacsi@gmail.com>
2017-03-26 15:56:58 +0530

    add slick slider

--- js/main.js
+++ js/main.js
@@ -3,27 +3,42 @@
 //console.log(mySelf);
 //console.log(myObject);
 //console.log(JSON.stringify(myObject));
-var xhr=new XMLHttpRequest();
+var xhr = new XMLHttpRequest();
 console.log(xhr.readyState);
-xhr.open('GET','data.json',true);
-
-
+xhr.open('GET', 'data.json', true);
 xhr.send();
 console.log(xhr.readyState);
-xhr.onreadystatechange=function(){
+xhr.onreadystatechange = function () {
     console.log(xhr.readyState);
-
 }
-
-xhr.onload=function(){
+xhr.onload = function () {
     console.log("inside success");
- if(xhr.status==200){
-     var data=JSON.parse(xhr.responseText)
-    console.log(data);
- }
+    if (xhr.status == 200) {
+        var presidents = JSON.parse(xhr.responseText);
+        var presidentEl;
+        presidents.forEach(function (president) {
+            presidentEl = `
+                        <div class="card" style="width: 20rem;"> <img class="card-img-top" src=${president.url} alt=${getFullName(president)}>
+                            <div class="card-block">
+                                <h4 class="card-title">${getFullName(president)}</h4>
+                                <p class="card-text">${president.served}</p>
+                            </div>
+                        </div>`;
+            
+            document.getElementById('presidents').innerHTML = document.getElementById('presidents').innerHTML + presidentEl;
+        });
+    }
+    $('#presidents').slick({
+         infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 3
+    });
 }
-xhr.onerror=function(){
+
+function getFullName(president) {
+    return president.firstName + " " + president.middleName + " " + president.lastName;
+}
+xhr.onerror = function () {
     console.log("inside error");
     console.log(xhr.responseText);
 }
-
